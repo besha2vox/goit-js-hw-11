8492455a34fb97d6325c7b548aa28f3ca4f1fa2f
Serhii Revva <107750742+besha2vox@users.noreply.github.com>
2022-12-02 02:20:20 +0200

    add card-template.js

---
+++ src/js/card-template.js
@@ -0,0 +1,21 @@
+export function cardTemplate(props) {
+  const { webformatURL, tags, views, likes, comments, downloads } = props;
+
+  return `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${downloads}</b>
+      </p>
+    </div>
+  </div>`;
+}
